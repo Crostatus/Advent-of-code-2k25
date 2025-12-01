65668ae7d8b3eae8cbc67de9621f21ce8ba3318a
Andrea <andreaboccone33@gmail.com>
2025-12-01 13:50:47 +0100

    es1
    es1 complete

---
+++ es1/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LEN 1024
+
+int spin_dial(int* position, char direction, int spin, int dial_length) {
+
+    int factor = (direction == 'R') ? 1 : -1;
+    *position = (*position + (spin * factor)) % dial_length;
+
+    return *position == 0 ? 1 : 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Uso: %s <nome_file>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (!fp) {
+        perror("Errore nell'apertura del file");
+        return 1;
+    }
+
+    char buffer[MAX_LINE_LEN];
+    char direction;
+    int spin;
+
+    int position = 50;
+    int dial_length = 100;
+
+    int times_hit_zero = 0;
+    while (fgets(buffer, MAX_LINE_LEN, fp) != NULL) {
+        if (sscanf(buffer, " %c%d", &direction, &spin) != 2) {
+            fprintf(stderr, "Riga non valida: %s", buffer);
+        }        
+        printf("Letto -> carattere: %c, numero: %d\n", direction, spin);
+        times_hit_zero+= spin_dial(&position, direction, spin, dial_length);
+    }
+    printf("Totale volte raggiunto zero: %d\n", times_hit_zero);
+
+    fclose(fp);
+    return 0;
+}
+
+
